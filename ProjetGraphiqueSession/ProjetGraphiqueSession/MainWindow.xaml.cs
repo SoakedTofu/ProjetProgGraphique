20def6b34372e93a6f568dcc7834f8648600b64d
Charles O <95464631+SoakedTofu@users.noreply.github.com>
2024-11-20 14:08:15 -0500

    Test

--- ProjetGraphiqueSession/ProjetGraphiqueSession/MainWindow.xaml.cs
+++ ProjetGraphiqueSession/ProjetGraphiqueSession/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace ProjetGraphiqueSession
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            myButton.Content = "fgfg";
         }
     }
 }
